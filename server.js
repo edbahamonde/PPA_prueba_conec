e4ead25c8443cf58217e5798652cda1c5ae016a9
edbahamonde <49206926+edbahamonde@users.noreply.github.com>
2022-01-12 15:19:53 -0500

    ddd
    ddd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('[{"id":2}]'))
+app.get('/', (req, res) => res.send('[{name: string;cedula: string;email: string;persona: string;curso: string;}]'))
 
 app.listen(3000);
 console.log('Servidor en puerto 3000')
