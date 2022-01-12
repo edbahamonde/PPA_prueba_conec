f7aa73df204fedb1fe0995768a4c0a6321ff0c2b
edbahamonde <49206926+edbahamonde@users.noreply.github.com>
2022-01-12 15:16:30 -0500

    as
    sss

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('[{"name":yolo}])'))
+app.get('/', (req, res) => res.send('[{"id":2}]'))
 
 app.listen(3000);
 console.log('Servidor en puerto 3000')
