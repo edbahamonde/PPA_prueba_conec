1795a260d59075dc5d6570a682a2e6e9c779be81
edbahamonde <49206926+edbahamonde@users.noreply.github.com>
2022-01-12 14:57:54 -0500

    prueba
    Prueba de datos

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hi: v'))
+app.get('/', (req, res) => res.send('[{"name":yolo}])'))
 
 app.listen(3000);
 console.log('Servidor en puerto 3000')
